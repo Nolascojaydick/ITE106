44cb7b0b6aeb0eec9de3ea11154f01881e001e15
Nolascojaydick <jaydickj@gmail.com>
2024-10-14 14:58:15 +0800

    Delete Bicycle Program directory

--- Bicycle Program/BicycleMain.java
+++
@@ -1,9 +0,0 @@
-public class BicycleMain
-{
-	public static void main(String[] args) {
-		Bicycle sportsBicycle = new Bicycle1();
-		
-		System.out.println(sportsBicycle.gear);
-		sportsBicycle.braking();
-	}
-}
